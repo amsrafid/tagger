4141e760880ed0f90b17e8bea50e094265e5b60b
amsrafid <amsrafid@gmail.com>
2020-07-19 18:36:41 +0600

    Implement multilevel if condition

--- src/Html/Condition.php
+++ src/Html/Condition.php
@@ -11,6 +11,53 @@ namespace Html;
  */
 class Condition
 {
+	private static function conditionValidate($condition, $bracketSet, $leftPosition, $rightPosition)
+	{
+		$bracketOut = [];
+		while($leftPosition < $rightPosition) {
+			if($condition[$leftPosition] != '('){
+				if($condition[$leftPosition] == ')') {
+					throw new \Exception("Bracket pattern is mismatched.");
+				}
+
+				$bracketOut [] = $condition[$leftPosition];
+				$leftPosition++;
+			} else{
+				if(isset($bracketSet[$leftPosition])) {
+					$bracketOut [] = self::conditionValidate(
+						$condition,
+						$bracketSet,
+						$leftPosition + 1,
+						$bracketSet[$leftPosition]
+					);
+				}
+				else {
+					throw new \Exception("Bracket pattern is mismatched.");
+				}
+
+				$leftPosition = $bracketSet[$leftPosition] + 1;
+			}
+		}
+
+		return self::replaceConditionals(implode('', $bracketOut));
+	}
+
+	private static function replaceConditionals($string)
+	{
+		$planeSet = [];
+		for($i = 0; $i < \strlen($string); $i++) {
+			if($string[$i] == '&') {
+				\array_pop($planeSet);
+				$planeSet [] = (($string[$i-1] + $string[$i+1]) == 2) ? 1 : 0;
+				$i++;
+			} else if($string[$i] != '|') {
+				$planeSet [] = $string[$i];
+			}
+		}
+
+		return \in_array(1, $planeSet) ? 1 : 0;
+	}
+
 	/**
 	 * Match condition from string
 	 * 
@@ -19,14 +66,33 @@ class Condition
 	 */
 	public static function match ($condition)
 	{
-	    preg_match('/.*(.*?)\s(.*?)\s(.*?)/sU', $condition, $condition_matches);
+		$simplified = self::simplifyCondition($condition);
+		
+		$first = 0;
+		$stack = [];
+		$bracketPositions = [];
 
-	    $cond_left = self::trimSpecialCharFromMatch($condition_matches, 1);
-	    $cond_right = self::trimSpecialCharFromMatch($condition_matches, 3);
+		for($i = 0; $i < strlen($simplified); $i++) {
+			if($simplified[$i] == "(") {
+				$stack [] = $i;
+			} else if ($simplified[$i] == ")") {
+					$bracketPositions[end($stack)] = $i;
+					array_pop($stack);
+			}
+		}
 
-	    $operator = $condition_matches[2];
+		return self::conditionValidate($simplified, $bracketPositions, 0, strlen($simplified));
 
-	    return self::overloadOperator($cond_left, $operator, $cond_right);
+			/*
+				preg_match('/.*(.*?)\s(.*?)\s(.*?)/sU', $condition, $condition_matches);
+
+				$cond_left = self::trimSpecialCharFromMatch($condition_matches, 1);
+				$cond_right = self::trimSpecialCharFromMatch($condition_matches, 3);
+
+				$operator = $condition_matches[2];
+
+				return self::overloadOperator($cond_left, $operator, $cond_right);
+			*/
 	}
 
 	/**
@@ -87,5 +153,44 @@ class Condition
 			return trim(str_replace(['$','()'],'', $match[$offset]));
 
 		throw new \Exception("Conditional statement must need three parts with ({left value} {operator} {right value}) format");
+	}
+
+	private static function simplifyCondition($condition)
+	{
+		$simplified = [];
+		$position = 0;
+		$perCondition = ['', '', ''];
+		$firstPosition = 0;
+
+		for ($i = 0; $i < strlen($condition); $i++) {
+			if(preg_match('/[a-zA-Z0-9!=<>]/', $condition[$i])) {
+				$perCondition[$position] .= $condition[$i];
+				$firstPosition = $perCondition[0] ? $firstPosition : $i;
+			} else if ($perCondition[0] != "" &&
+				(($condition[$i] == ' ') ||
+					(\in_array($condition[$i], [' ', ')']) && $position == 2))
+			) {
+				$position++;
+			}
+
+			if($position > 2 || ($i == strlen($condition) - 1)) {
+				$simplified [] = self::overloadOperator($perCondition[0], $perCondition[1], $perCondition[2]) ? 1 : 0;
+				$perCondition = ['', '', ''];
+				$position = 0;
+				$firstPosition = 0;
+			}
+
+			if (in_array($condition[$i], ['|', '&'])) {
+				$simplified [] = $condition[$i];
+				$i++;
+			} else if (in_array($condition[$i], ['(', ')'])) {
+				$simplified [] = $condition[$i];
+			}
+		}
+		
+		if($simplified)
+			return \implode('', $simplified);
+
+		throw new \Exception("Condition pattern '".$condition."' is mismatched.");
 	}
 }
